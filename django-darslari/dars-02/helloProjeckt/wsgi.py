27c14584b8d00570af5af47850b60f015eba0588
Muhammadziyo Begaliyev <muhammadziyobegaliyev872@gmail.com>
2025-11-21 02:25:24 +0500

    Hello World , manage.py , runserver bln ishlash

---
+++ django-darslari/dars-02/helloProjeckt/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for helloProjeckt project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloProjeckt.settings')
+
+application = get_wsgi_application()
