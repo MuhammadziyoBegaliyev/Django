347238c0cc5f451ee4a800f04307c99e46ff78af
Muhammadziyo Begaliyev <muhammadziyobegaliyev872@gmail.com>
2025-12-01 20:27:21 +0500

    02-blog

--- 02-blog/06-newBlog-users/config/settings.py
+++ 02-blog/06-newBlog-users/config/settings.py
@@ -128,6 +128,10 @@ STATICFILES_DIRS = [
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
+#EMAIL backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
